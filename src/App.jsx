97fd06aba052f25760eaaeadb46ffe7562da4681
Aparicio Junior <apariciojunior11@gmail.com>
2023-06-21 14:20:07 -0300

    testes com componente Chamada

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,20 @@ import Main from "./components/main/Main.jsx";
 import Header from "./components/header/Header.jsx";
 import Footer from "./components/footer/Footer.jsx";
 import Whatsapp from "./components/tools/Whatsapp.jsx";
+import Chamada from "./components/main/sections/chamada/Chamada.jsx"
 
 export default function App() {
 	return (
+		
 		<Fragment>
+			<Chamada />
+		</Fragment>
+		
+		/* <Fragment>
 			<Header />
 			<Main />
 			<Footer />
 			<Whatsapp />
-		</Fragment>
+		</Fragment> */
 	);
 }
