4a4b5ff393896bd08c102f4ba4ce554e9b9ab5ac
Aparicio Junior <apariciojunior11@gmail.com>
2023-06-21 21:36:59 -0300

    update componente conheca

--- src/components/main/sections/conheca/Conheca.jsx
+++ src/components/main/sections/conheca/Conheca.jsx
@@ -1,17 +1,20 @@
-
+import React from 'react';
+import styles from "./conheca.module.css"
 
 export default function Conheca() {
-	return (
-		<div>
-			<div className={styles.video-container}>
-      			<div className={styles.content-container}>
-       			<h1 className={styles.title}>Título</h1>
-        		<p className={styles.subtitle}>Subtítulo</p>
-      	</div>
+  return (
+    <div>
+      <div className={styles['video-container']}>
+        
+        <div className={styles['content-container']}>
+          <p className={styles['titulo']}>Conheça</p>
+		      <p className={styles['titulo2']}>PPUSE</p>
+          <p className={styles['subtitulo']}>Alugar um consultório odontológico pode ser mais viável do que parece.</p>
+        </div>
 
-      <video className="video" src="/caminho/para/o/video.mp4" controls />
-
-    	</div>
-		</div>
-	);
+        <video className={styles['video']} src="/caminho/para/o/video.mp4" controls />
+      
+      </div>
+    </div>
+  );
 }
